62f62b79eb96fb391630be1716c5d61fa619471c
Furkan <furkan-ay@outlook.com>
2016-12-06 18:04:50 +0300

    added character movement

--- scripts/ufo.gd
+++ scripts/ufo.gd
@@ -1,6 +1,38 @@
 extends RigidBody2D
 
+var speed_x = 200
+var speed_y = 200
+var current_speed = Vector2(0, 0)
+var acceleration = 10
+
+var btn_up = Input.is_action_pressed("btn_up")
+var btn_down = Input.is_action_pressed("btn_down")
+var btn_left = Input.is_action_pressed("btn_left")
+var btn_right = Input.is_action_pressed("btn_right")
+
+func move(speed_x, speed_y, acceleration, delta):
+	current_speed.x = lerp(current_speed.x, speed_x, acceleration * delta)
+	current_speed.y = lerp(current_speed.y, speed_y, acceleration * delta)
+	set_linear_velocity(current_speed)
 
 
 func _ready():
 	set_fixed_process(true)
+
+func _fixed_process(delta):
+	print(get_linear_velocity())
+	btn_up = Input.is_action_pressed("btn_up")
+	btn_down = Input.is_action_pressed("btn_down")
+	btn_left = Input.is_action_pressed("btn_left")
+	btn_right = Input.is_action_pressed("btn_right")
+	
+	if btn_left:
+		move(-speed_x, 0, acceleration, delta)
+	if btn_right:
+		move(speed_x, 0, acceleration, delta)
+	if btn_up:
+		move(0, -speed_y, acceleration, delta)
+	if btn_down:
+		move(0, speed_y, acceleration, delta)
+	#else:
+		#move(0, acceleration, delta)
